afd1d89790780ec40ffa2b348e5eedf91b812b77
Andrea Palomeque <andrea@asteroidtechs.com>
2024-02-21 16:31:29 -0300

    installed webgazer library

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,9 @@ import SubmitButton from "@/components/SubmitButton";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-hablalo_black">
-      <h1 className="text-4xl font-bold text-white mb-8">Bienvenido</h1>
+      <h1 className="text-4xl font-bold text-white mb-8">
+        Testing WebGazer.js
+      </h1>
       <Link href="/form">
         <SubmitButton text="Empezar" />
       </Link>
